91eaa4b390bec682aead44e80a39dabfac9d317d
Lucas Cardoso da Silva <lucascardoso.ds16@gmail.com>
2022-08-29 23:05:59 -0300

    Commit no desktop

--- aula1-ide/src/br/org/serratec/diversos/Exemplo3.java
+++ aula1-ide/src/br/org/serratec/diversos/Exemplo3.java
@@ -5,10 +5,11 @@ public class Exemplo3 {
 	public static void main(String[] args) {
 		// CTRL + ALT + SETAS - Replica a linha
 
-		System.out.println("Programação em Java - Aula 300000900900");
 		System.out.println("Programação em Java - Aula 100000900900");
 		System.out.println("Programação em Java - Aula 200090900900");
+		System.out.println("Programação em Java - Aula 300000900900");
 		System.out.println("Programação em Java - Aula 400000900900");
+		System.out.println("Programação em Java - Aula 500000900900");
 	}
 
 }
